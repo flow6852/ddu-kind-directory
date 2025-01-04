68c72fafc0c66d0a8d3028405a5116616769c8ac
flow6852 <flow6852math@gmail.com>
2025-01-04 16:24:50 +0900

    init

---
+++ README.md
@@ -0,0 +1,23 @@
+# ddu-kind-directory
+
+Vim type kind for directory.
+
+## Required
+
+### denops.vim
+
+https://github.com/vim-denops/denops.vim
+
+### ddu.vim
+
+https://github.com/Shougo/ddu.vim
+
+### ddu-source-directory
+
+https://github.com/flow6852/ddu-source-directory.vim
+
+## Demo
+
+## Configuration
+
+Please read EXAMPLE in doc/ddu-kind-directory.txt.
---
+++ denops/@ddu-kinds/directory.ts
@@ -0,0 +1,80 @@
+import {
+  ActionFlags,
+  Actions,
+  BaseKind,
+  DduItem,
+} from "https://deno.land/x/ddu_vim@v3.10.1/types.ts";
+
+type Params = Record<never, never>;
+
+export class Kind extends BaseKind<Params> {
+  override actions: Actions<Params> = {
+    open: async (args: { denops: Denops; items: DduItem[] }) => {
+      // Save current position.
+      await args.denops.cmd("normal! m`");
+
+      for (const i of args.items) {
+        await args.denops.cmd(
+          "edit " + i.action.path,
+        );
+      }
+      return ActionFlags.None;
+    },
+
+    chain: async (args: { denops: Denops; items: DduItem[] }) => {
+      const sources = await Promise.all(args.items.map(async (item) => {
+        return await updateSourcePath(item, args.options.sources);
+      }));
+      await args.denops.call("ddu#start", {
+        name: args.options.name,
+        push: false,
+        sources: sources.flat(),
+      });
+      return ActionFlags.None;
+    },
+  };
+
+  override async getPreviewer(args: {
+    denops: Denops;
+    item: DduItem;
+    actionParams: BaseParams;
+    previewContext: PreviewContext;
+  }): Promise<Previewer | undefined> {
+    const action = args.item.action as ActionData;
+    if (!action) {
+      return undefined;
+    }
+
+    // const param = ensure(args.actionParams, is.Record) as PreviewOption;
+    const dirs = [];
+    for await (const file of Deno.readDir(action.path)) {
+      dirs.push(file.name);
+    }
+
+    return {
+      kind: "nofile",
+      contents: dirs,
+    };
+  }
+  override params(): Params {
+    return {};
+  }
+}
+
+const updateSourcePath = async (item: DduItem, sources) => {
+  const action = item?.action as ActionData;
+  let retSources = [];
+
+  // NOTE: Deno.stat() may be failed
+  try {
+    const path = action.path;
+    if ((await Deno.stat(path)).isDirectory) {
+      for (let source of sources) {
+        retSources.push({ ...source, ...{ options: { path: path } } });
+      }
+    }
+  } catch (_e: unknown) {
+    // Ignore
+  }
+  return retSources;
+};
